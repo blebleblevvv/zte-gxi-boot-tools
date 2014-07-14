859c8ef8a2428e7ab0979849a32c0e98db976c31
Pavel Moravec <pavel.moravec@vsb.cz>
2014-07-14 13:12:59 +0200

    Add static-linked binary and its NDK compilation

--- build_blobs.sh
+++ build_blobs.sh
@@ -12,9 +12,10 @@ export NDK
 export SYSROOT=$NDK/platforms/android-15/arch-x86
 export CC="$NDK/toolchains/x86-4.6/prebuilt/linux-x86_64/bin/i686-linux-android-gcc --sysroot=$SYSROOT"
 
-make clean all gxi_dump_images
+make clean all gxi_dump_images gxi_dump_images_s
 mkdir medfield_bin 2> /dev/null
 mv gxi_pack medfield_bin/pack
 mv gxi_unpack medfield_bin/unpack
 mv gxi_dump_images medfield_bin/dump_images
+mv gxi_dump_images_s medfield_bin/dump_images_s
 make clean
